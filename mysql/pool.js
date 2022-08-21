56b07aea5fff04d14c408465b1dcd71738c16fc7
Yuzi0201 <2205816944@qq.com>
2022-08-21 13:58:36 +0800

    product ver

--- mysql/pool.js
+++ mysql/pool.js
@@ -1,19 +1,19 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: '43.156.106.129',
-    port: 65514,
+    host: 'database',
+    port: 3306,
     user: 'root',
     password: '7hHeKT5jJxVGsU',
     database: 'MaxPort'
 })
 
-let query = (sql, params,callback) => {
+let query = (sql, params, callback) => {
     pool.getConnection((err, connection) => {
         if (err) { return callback(err) }
         connection.query(sql, params, (err, res) => {
             connection.release();
-            console.log(sql,err, res);
+            console.log(sql, err, res);
             if (err) { return callback(err) }
             return callback(err, res)
         })
